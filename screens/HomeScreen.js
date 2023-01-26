f22483d5aa3ba5681a4d82fd5c2546d309f0ed31
jimmy24599 <ahmedg24599@gmail.com>
2023-01-26 17:23:40 +0400

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,70 @@
+import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from "@env";
+import { TextInput } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { setOrigin, setDestination } from '../slices/navSlice';
+import NavFavourites from '../components/NavFavourites';
+
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+    return (
+      <SafeAreaView style={tw`bg-white h-full`}>
+    <View style={tw`p-5`}>
+      <Image
+      style= {{width:100, height:100, resizeMode:'contain'}}
+      source ={{
+        uri: "https://links.papareact.com/gzs",
+      }}/>
+      <GooglePlacesAutocomplete
+      enablePoweredByContainer={false}
+      placeholder='Where from?'
+      styles={{
+        
+        container: {
+          flex: 0,
+        },
+
+        textInput:{
+            fontSize: 18,
+        }
+      }}
+      onPress={(data, details = null) => {
+        dispatch(
+          setOrigin({
+            location: details.geometry.location,
+            description: data.description,
+          })
+        );
+
+          dispatch(setDestination(null))
+      }}  
+      fetchDetails={true}
+      returnKeyType = {"search"}
+      minLength={2}
+      query={{
+        key: GOOGLE_MAPS_APIKEY,
+        language: 'en, ar'
+      }}
+      nearbyPlacesAPI='GooglePlacesSearch'
+      debounce = { 400 }/>
+
+
+    </View>
+    <NavOptions/>
+    <NavFavourites />
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    text: {
+        color: "blue",
+    },
+})
